6eacc27f95b2377b17e93fe442f4f2ed09572d90
Kobanchik <okoba843@mail.ru>
2022-10-02 21:45:20 +0300

    rgs autotest Junit

--- ru/sdk/tests/SdkTests.java
+++ ru/sdk/tests/SdkTests.java
@@ -1,82 +1,92 @@
 package ru.sdk.tests;
 
-import org.junit.Test;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.WebDriver.Timeouts;
 
-import javax.swing.*;
+import java.time.Duration;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 
 public class SdkTests {
 
+    static WebDriver driver;
+    static JavascriptExecutor jse;
+
+    @BeforeAll
+    static void init() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        jse = (JavascriptExecutor) driver;
+
+    }
+
+
+
     @Test
     public void test() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        JavascriptExecutor jse = (JavascriptExecutor) driver;
 
-        driver.manage().window().maximize();
         driver.get("http://www.rgs.ru");
+        driver.manage().window().maximize();
 
-        WebElement forCompanies = driver.findElement(By.linkText("Компаниям"));
+        WebElement forCompanies = driver.findElement(By.xpath("//div[@id='__layout']/div/header/div/div/section/div/nav/ul/li[2]/a"));
         forCompanies.click();
 
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
-        driver.switchTo().frame(driver.findElement(By.id("fl-616371")));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+
+        driver.switchTo().frame(driver.findElement(By.className("flocktory-widget"))); //div[]
         WebElement annoy = driver.findElement(By.className("widget__close"));
         annoy.click();
 
         driver.switchTo().defaultContent();
 
-        List<WebElement> padding= driver.findElements(By.className("padding"));
-        padding.get(1).click();
+        List<WebElement> padding= driver.findElements(By.xpath("//div[@id='__layout']/div/header/div/div/section[2]/div/div/div/div/ul/li[2]/span"));
+        padding.get(0).click();
 
 
-        WebElement health = driver.findElement(By.linkText("Добровольное медицинское страхование"));
+        WebElement health = driver.findElement(By.xpath("//div[@id='__layout']/div/header/div/div[2]/div[2]/div/div/div/section/div/div[2]/div/div/div/ul/li[3]/a"));
         health.click();
 
-        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-        Boolean isPresent = driver.findElements(By.linkText("Добровольное медицинское страхование")).size() > 0;
-        System.out.println(isPresent);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
+        Assertions.assertEquals("Добровольное медицинское страхование", driver.findElement(By.xpath("//h1[@class='title word-breaking title--h2'][contains(.,'Добровольное медицинское страхование')]")).getAttribute("textContent"));
 
         WebElement form = driver.findElement(By.className("action-item"));
         form.click();
 
-        WebElement nameInput = driver.findElement(By.name("userName"));
+        WebElement nameInput = driver.findElement(By.xpath("//div[@id='__layout']/div/main/div/div/div[4]/section/div/div/div/div/div/div[2]/div/form/div/div/div/div/div/input"));
         String name = "qwertyqwerty qwertyqwerty qwertyqwerty";
         nameInput.sendKeys(name);
-        System.out.println(name.equals(nameInput.getAttribute("value")));
+        Assertions.assertEquals(name, nameInput.getAttribute("value"));
 
         String telephone = "+7 (999) 999-9999";
-        WebElement telInput = driver.findElement(By.name("userTel"));
+        WebElement telInput = driver.findElement(By.xpath("//div[@id='__layout']/div/main/div/div/div[4]/section/div/div/div/div/div/div[2]/div/form/div/div[2]/div/div/div/input"));
         telInput.sendKeys("9999999999");
-        System.out.println(telephone.equals(telInput.getAttribute("value")));
+        Assertions.assertEquals(telephone, telInput.getAttribute("value"));
 
         String email = "234234234234";
-        WebElement mailInput = driver.findElement(By.name("userEmail"));
+        WebElement mailInput = driver.findElement(By.xpath("//div[@id='__layout']/div/main/div/div/div[4]/section/div/div/div/div/div/div[2]/div/form/div/div[3]/div/div/div/input"));
         mailInput.sendKeys(email);
-        System.out.println(email.equals(mailInput.getAttribute("value")));
+        Assertions.assertEquals(email, mailInput.getAttribute("value"));
 
-        String adress = "234234234234";
-        WebElement adressInput = driver.findElement(By.className("vue-dadata__input"));
-        adressInput.sendKeys(adress);
-        System.out.println(adress.equals(adressInput.getAttribute("value")));
+        String address = "234234234234";
+        WebElement addressInput = driver.findElement(By.xpath("//div[@id='__layout']/div/main/div/div/div[4]/section/div/div/div/div/div/div[2]/div/form/div/div[4]/div/div/div/div/div/div/input"));
+        addressInput.sendKeys(address);
+        Assertions.assertEquals(address, addressInput.getAttribute("value"));
 
         jse.executeScript("document.getElementsByClassName('checkbox')[0].click()");
 
         jse.executeScript("document.getElementsByClassName('form__button-submit')[0].click()");
 
-        System.out.println(mailInput.getAttribute("class").contains("error-field"));
-
-        driver.quit();
-
+        Assertions.assertTrue(mailInput.getAttribute("class").contains("error-field"));
     }
 
-
-}
+    @AfterAll
+    static void finish() {
+        driver.quit();
+    }
+    }
